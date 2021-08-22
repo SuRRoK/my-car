fe44c3d84b20e7967c14697f8dc12659910d43e1
surrok <surrok6@gmail.com>
2021-08-22 09:58:33 +0300

    Inti project

---
+++ src/Infrastructure/di.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure;
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $di): void {
+    $services = $di
+        ->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+
+//    $services->set('my_business_feature.some_service', \stdClass::class);
+};
